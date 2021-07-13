0f805bbce1e421248404f806cf40ecf9540c09a7
WahidinAji <a17wahidin@gmail.com>
2021-07-13 14:04:02 +0700

    add model and migration for DiaryNote

---
+++ database/migrations/2021_07_13_065844_create_diary_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiaryNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diary_notes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('context');
+            $table->string('accent');
+            $table->text('english');
+            $table->text('indonesia');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diary_notes');
+    }
+}
